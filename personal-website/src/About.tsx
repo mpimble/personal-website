a84a72e43be96c0105d32a334e137e6a7d527f7d
mpimble <pimblemichael@yahoo.com>
2025-06-13 15:59:40 -0400

    Removed Navbar explicitly from Home and About pages

--- personal-website/src/About.tsx
+++ personal-website/src/About.tsx
@@ -2,13 +2,10 @@ import Navbar from "./Navbar"
 
 function About() {
     return (
-        <>
-            <Navbar></Navbar>
-            <div>
-                <h1>About This Site</h1>
-                <p>This website is merely a work-in-progress right now. Come back later for updates.</p>
-            </div>
-        </>
+        <div>
+            <h1>About This Site</h1>
+            <p>This website is merely a work-in-progress right now. Come back later for updates.</p>
+        </div>
     )
 }
 
