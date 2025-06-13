d9b526fd51bb65d7bc2056fd69a97abce82db6c8
mpimble <pimblemichael@yahoo.com>
2025-06-13 14:26:39 -0400

    Added bare bones about component

--- personal-website/src/main.tsx
+++ personal-website/src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter } from 'react-router-dom'
+import About from './About.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/about', element: <About /> }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
