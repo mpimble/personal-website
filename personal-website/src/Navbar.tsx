aea87e41ea6ca5440e9054ab71432b90f4b361f3
mpimble <pimblemichael@yahoo.com>
2025-06-13 15:26:20 -0400

    Tweaks to navbar
    Added title text that is vertically aligned, aligned all links to the right

--- personal-website/src/Navbar.tsx
+++ personal-website/src/Navbar.tsx
@@ -4,6 +4,7 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className="navbar">
+            <div className="navbar-title"><Link to='/'>Personal Website</Link></div>
             <ul className="nav-links">
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
