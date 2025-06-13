bee334db7f44f99d0c03a0ae9a05e7c90c9954b2
mpimble <pimblemichael@yahoo.com>
2025-06-13 14:39:17 -0400

    Added routing for NotFoundPage

--- personal-website/src/main.tsx
+++ personal-website/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import About from './About.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NotFoundPage from './NotFoundPage.tsx'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
-  { path: '/about', element: <About /> }
+  { path: '/about', element: <About /> },
+  { path: '*', element: <NotFoundPage /> }
 ]);
 
 createRoot(document.getElementById('root')!).render(
