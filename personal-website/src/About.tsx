3ed758719ad5d9beefc8abd73f24e80926627b38
mpimble <pimblemichael@yahoo.com>
2025-06-13 15:06:05 -0400

    Created a Navbar

--- personal-website/src/About.tsx
+++ personal-website/src/About.tsx
@@ -1,9 +1,14 @@
+import Navbar from "./Navbar"
+
 function About() {
     return (
-        <div>
-            <h1>About This Site</h1>
-            <p>This website is merely a work-in-progress right now. Come back later for updates.</p>
-        </div>
+        <>
+            <Navbar></Navbar>
+            <div>
+                <h1>About This Site</h1>
+                <p>This website is merely a work-in-progress right now. Come back later for updates.</p>
+            </div>
+        </>
     )
 }
 
--- personal-website/src/main.tsx
+++ personal-website/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import About from './About.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import NotFoundPage from './NotFoundPage.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
