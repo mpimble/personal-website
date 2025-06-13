15d97a9dc4d50538a13304366eb7286c588bdf83
mpimble <pimblemichael@yahoo.com>
2025-06-13 14:45:04 -0400

    Update About.tsx

--- personal-website/src/About.tsx
+++ personal-website/src/About.tsx
@@ -1,6 +1,7 @@
 function About() {
     return (
         <div>
+            <h1>About This Site</h1>
             <p>This website is merely a work-in-progress right now. Come back later for updates.</p>
         </div>
     )
