0ef76cb02402b5b7d8e01dac1cf3c5c4f298679d
mpimble <pimblemichael@yahoo.com>
2025-06-16 10:46:06 -0400

    Update homepage text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Link } from 'react-router'
 
 const App = () => {
   return (
     <div>
-
+      <p>Here, you can <Link to="/about">learn more about Michael Pimble</Link>, <Link to="/projects">look at his projects</Link>, or go directly to Regression Helper.</p>
     </div>
   )
 }
